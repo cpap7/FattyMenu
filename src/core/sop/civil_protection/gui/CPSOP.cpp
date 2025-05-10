d8b576f825369668012adbf9f66f91c5d918d6f0
cpap7 <mjajarmi2000@outlook.com>
2025-05-10 00:17:58 -0400

    Clean up comments & fix method signatures

--- src/core/sop/civil_protection/gui/CPSOP.cpp
+++ src/core/sop/civil_protection/gui/CPSOP.cpp
@@ -1,7 +1,7 @@
 #include "CPSOP.h"
 
 /* Displays ASCII art of the Civil Protection symbol */
-void CPSOP::DisplayLogo() {
+void CPSOP::DisplayCPLogo() {
 	// Could probably format this better, but too lazy to bother 
 	ImGui::Text("							----   -----");
 	ImGui::Text("							----   -----");
@@ -47,8 +47,8 @@ void CPSOP::DisplayLogo() {
 
 }
 
-/* TODO: fix lambda functions and streamline everything to use template structure outlined in CPSOP.h
-	CPSOP::DisplayList(codes_list, [] (const CCode& code) {
+/* TODO: fix lambda functions and streamline everything to use template structure outlined in GUIUtilities.h
+	CPSOP::DisplayCPList(codes_list, [] (const CCode& code) {
 		ImGui::TextWrapped("%s: %s", code.GetCodeName(), code.GetCodeDescription());
 
 		if (code.IsViolationCode()) {
@@ -79,7 +79,7 @@ void CPSOP::DisplayLogo() {
 
 // Function for displaying radio codes
 // @param codes_list -> vector containing a list of radio codes to be looped through and displayed
-void CPSOP::DisplayCodes(const std::vector<CCode> codes_list) {
+void CPSOP::DisplayCPCodes(const std::vector<CCode> codes_list) {
 	// Loop through the radio codes
 	for (const auto& code : codes_list) {
 		// Check if the code is an override code, which contains lists of strings
@@ -115,7 +115,7 @@ void CPSOP::DisplayCodes(const std::vector<CCode> codes_list) {
 
 // Function for displaying civic point and ration rewards for public service details and civic deeds
 // @param reward_list -> vector containing each type of civic duty that a unit can issue a reward for, which is looped through and displayed
-void CPSOP::DisplayRewardInfo(const std::vector<CCivicReward> reward_list) {
+void CPSOP::DisplayCPRewardInfo(const std::vector<CCivicReward> reward_list) {
 	// Loop through the rewards
 	for (const auto& reward : reward_list) {
 		// Display them as bullet text
@@ -128,7 +128,7 @@ void CPSOP::DisplayRewardInfo(const std::vector<CCivicReward> reward_list) {
 	}
 }
 
-void CPSOP::DisplayInteractionDirectives(const std::vector<CCivilStatus> civil_status_list) {
+void CPSOP::DisplayCPInteractionDirectives(const std::vector<CCivilStatus> civil_status_list) {
 	// Loop through the different status types
 	for (const auto& civil_status : civil_status_list) {
 		// Display the name of the status type, and the civic point range
@@ -149,7 +149,7 @@ void CPSOP::DisplayInteractionDirectives(const std::vector<CCivilStatus> civil_s
 
 // Function for displaying Combine terminology
 // @param terms_list -> vector containing a list of terms to be looped through and displayed
-void CPSOP::DisplayTerms(const std::vector<CTerm> terms_list) {
+void CPSOP::DisplayCPTerms(const std::vector<CTerm> terms_list) {
 	// Loop through the terms in the vector list
 	for (const auto& term : terms_list) {
 		// Display them as bullet text
@@ -162,7 +162,7 @@ void CPSOP::DisplayTerms(const std::vector<CTerm> terms_list) {
 
 // Function for displaying Politi-Schedule
 // @param politi_schedule_list -> vector containing a list of events to be looped through and displayed
-void CPSOP::DisplayPolitiSchedule(const std::vector<CPolitiSchedule> politi_schedule_list) {
+void CPSOP::DisplayCPPolitiSchedule(const std::vector<CPolitiSchedule> politi_schedule_list) {
 	// Create the table for the schedule
 	ImGui::BeginTable("PolitiSchedule", 4, ImGuiTableFlags_Resizable | ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg);
 	
@@ -195,27 +195,16 @@ void CPSOP::DisplayPolitiSchedule(const std::vector<CPolitiSchedule> politi_sche
 	}
 
 	ImGui::EndTable();
-	
-	/* Old janky version
-	ImGui::Text("%-15s %-15s %-30s %-15s", "TIME START", "TIME END", "MANDATE", "LENGTH");
-    ImGui::Separator();
-
-    // Loop through the Politi-Schedule events
-    for (const auto& event : politi_schedule_list) {
-        // Display start time, end time, mandate, and time length and align them
-        ImGui::Text("%-15s %-15s %-30s %-15s", event.GetTimeStart(), event.GetTimeEnd(), event.GetMandate(), event.GetTimeLength());
-    }
-	*/
 }
 
 
 // Function for displaying mandate and assignment duties
 // @param duties_list -> vector containing a list of duties to be looped through and displayed
-void CPSOP::DisplayDuties(const std::vector<CAssignment> duties_list) {
+void CPSOP::DisplayCPDuties(const std::vector<CAssignment> duties_list) {
 	// Loop through the duties in the vector list
 	for (const auto& duty : duties_list) {
 		// Display assignment name and the # of required units
-		ImGui::TextWrapped("%s\n%s", duty.GetAssignmentName(), duty.GetUnitsRequired());
+		ImGui::TextWrapped("%s\n%s", duty.GetAssignmentName(), duty.GetCPUnitsRequired());
 
 		// Display each string description
 		for (const auto& description : duty.GetAssignmentDescription()) {
@@ -229,7 +218,7 @@ void CPSOP::DisplayDuties(const std::vector<CAssignment> duties_list) {
 
 // Function for displaying contraband index
 // @param contraband_list -> vector containing a list of contraband, their verdict codes, and examples to be looped through and displayed
-void CPSOP::DisplayContrabandIndex(const std::vector<CContraband> contraband_list) {
+void CPSOP::DisplayCPContrabandIndex(const std::vector<CContraband> contraband_list) {
 	// Loop through the contraband categories
 	for (const auto& category : contraband_list) {
 		// Display the violation code's name, description
@@ -251,111 +240,111 @@ void CPSOP::RenderCivilProtectionSOP() {
 	// Collapsing headers render info once the user clicks on them
 	if (ImGui::CollapsingHeader("<:: DISPLAY CIVIL PROTECTION LOGO ::>")) {
 		// Print the ASCII art logo
-		DisplayLogo();
+		DisplayCPLogo();
 	}
 
 	// Display the code index, render the subheaders via the inline helper function
-	RenderSOPSection("<:: CODE INDEX ::>", [] {
+	GUIUtils::RenderSOPSection("<:: CODE INDEX ::>", [] {
 		// Clicking the first header displays these options
 		if (ImGui::CollapsingHeader("<:: View Abbreviations ::>")) {
-			DisplayCodes(CPSOPLookupTables::abbreviation_list);
+			DisplayCPCodes(CPSOPLookupTables::abbreviation_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Response Codes ::>")) {
-			DisplayCodes(CPSOPLookupTables::response_code_list);
+			DisplayCPCodes(CPSOPLookupTables::response_code_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View 11- Codes ::>")) {
-			DisplayCodes(CPSOPLookupTables::eleven_code_list);
+			DisplayCPCodes(CPSOPLookupTables::eleven_code_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View 10- Codes ::>")) {
-			DisplayCodes(CPSOPLookupTables::ten_code_list);
+			DisplayCPCodes(CPSOPLookupTables::ten_code_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Code Violations ::>")) {
 			ImGui::Text("Anti-Civil Level 5\n -> Verbal warning and/or citation\n");
-			DisplayCodes(CPSOPLookupTables::anti_civil_level_5_violations);
+			DisplayCPCodes(CPSOPLookupTables::anti_civil_level_5_violations);
 
 			ImGui::Text("\nAnti-Civil Level 1\n -> Prosecution: Sentencing is discretionary\n -> -10 civic points, 4 min sentence time\n");
-			DisplayCodes(CPSOPLookupTables::anti_civil_level_1_violations);
+			DisplayCPCodes(CPSOPLookupTables::anti_civil_level_1_violations);
 
 			ImGui::Text("\nSocio-Endangerment Level 5\n -> Prosecution: Dispatch approval required before proceeding\n -> -25 civic points, 6 min sentence time\n");
-			DisplayCodes(CPSOPLookupTables::socio_endangerment_level_5_violations);
+			DisplayCPCodes(CPSOPLookupTables::socio_endangerment_level_5_violations);
 
 			ImGui::Text("\nSocio-Endangerment Level 1\n -> Prosecution: Dispatch approval required before proceeding\n -> -25 civic points, 8 min sentence time\n");
-			DisplayCodes(CPSOPLookupTables::socio_endangerment_level_1_violations);
+			DisplayCPCodes(CPSOPLookupTables::socio_endangerment_level_1_violations);
 		}
 		if (ImGui::CollapsingHeader("<:: View Override Codes ::>")) {
-			DisplayCodes(CPSOPLookupTables::override_code_list);
+			DisplayCPCodes(CPSOPLookupTables::override_code_list);
 		}
 	});
 	
 	// Display civic point reward sections
-	RenderSOPSection("<:: CIVIC REWARD & INTERACTION INDEX ::>", [] {
+	GUIUtils::RenderSOPSection("<:: CIVIC REWARD & INTERACTION INDEX ::>", [] {
 		if (ImGui::CollapsingHeader("<:: View Public Service Details ::>")) {
-			DisplayRewardInfo(CPSOPLookupTables::public_service_detail_list);
+			DisplayCPRewardInfo(CPSOPLookupTables::public_service_detail_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Civic Deeds ::>")) {
-			DisplayRewardInfo(CPSOPLookupTables::civic_deed_list);
+			DisplayCPRewardInfo(CPSOPLookupTables::civic_deed_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Interaction Directives ::>")) {
-			DisplayInteractionDirectives(CPSOPLookupTables::citizen_interaction_directive_list);
+			DisplayCPInteractionDirectives(CPSOPLookupTables::citizen_interaction_directive_list);
 		}
 	});
 
 	// Display terminology index sections
-	RenderSOPSection("<:: TERMINOLOGY INDEX ::>", [] {
+	GUIUtils::RenderSOPSection("<:: TERMINOLOGY INDEX ::>", [] {
 		if (ImGui::CollapsingHeader("<:: View Protocol Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::protocol_list);
+			DisplayCPTerms(CPSOPLookupTables::protocol_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Action Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::action_list);
+			DisplayCPTerms(CPSOPLookupTables::action_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Action-Condition Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::action_condition_list);
+			DisplayCPTerms(CPSOPLookupTables::action_condition_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Hostile Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::hostile_list);
+			DisplayCPTerms(CPSOPLookupTables::hostile_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Equipment/Asset Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::equipment_asset_list);
+			DisplayCPTerms(CPSOPLookupTables::equipment_asset_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Organization Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::organization_list);
+			DisplayCPTerms(CPSOPLookupTables::organization_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Sociostability Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::sociostability_list);
+			DisplayCPTerms(CPSOPLookupTables::sociostability_list);
 		}
 		if (ImGui::CollapsingHeader("<:: View Area Terms ::>")) {
-			DisplayTerms(CPSOPLookupTables::area_list);
+			DisplayCPTerms(CPSOPLookupTables::area_list);
 		}
 
 	});
 
 	// Render politi schedule index
-	RenderSOPSection("<:: POLITI-SCHEDULE INDEX ::>", [] {
+	GUIUtils::RenderSOPSection("<:: POLITI-SCHEDULE INDEX ::>", [] {
 		// Display politi schedule
-		DisplayPolitiSchedule(CPSOPLookupTables::politi_schedule_event_list);
+		DisplayCPPolitiSchedule(CPSOPLookupTables::politi_schedule_event_list);
 	});
 
 	// Render duty index
-	RenderSOPSection("<:: DUTY INDEX ::> ", [] {
+	GUIUtils::RenderSOPSection("<:: DUTY INDEX ::> ", [] {
 		// Display mandate duties
 		if (ImGui::CollapsingHeader("<:: View Mandate Duties ::>")) {
-			DisplayDuties(CPSOPLookupTables::mandate_duties_list);
+			DisplayCPDuties(CPSOPLookupTables::mandate_duties_list);
 		}
 
 		// Display protection duties
 		if (ImGui::CollapsingHeader("<:: View Protection Duties ::>")) {
-			DisplayDuties(CPSOPLookupTables::protection_duties_list);
+			DisplayCPDuties(CPSOPLookupTables::protection_duties_list);
 		}
 
 		// Display duty expectations + TAC etiquette
 		if (ImGui::CollapsingHeader("<:: View Duty Expectations and TAC Etiquette ::>")) {
-			DisplayDuties(CPSOPLookupTables::miscellaneous_duties_list);
+			DisplayCPDuties(CPSOPLookupTables::miscellaneous_duties_list);
 		}
 	});
 
 	// Render contraband index
-	RenderSOPSection("<:: CONTRABAND INDEX ::> ", [] {
-		DisplayContrabandIndex(CPSOPLookupTables::contraband_list);
+	GUIUtils::RenderSOPSection("<:: CONTRABAND INDEX ::> ", [] {
+		DisplayCPContrabandIndex(CPSOPLookupTables::contraband_list);
 	});
 
 }
