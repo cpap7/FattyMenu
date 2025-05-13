d93314ca75cb6116212da7466dce997a2941db53
cpap7 <mjajarmi2000@outlook.com>
2025-05-12 22:09:35 -0400

    Adjust declarations' parameters to work with constructor for CCode object

--- src/core/sop/transhuman/definitions/TFCodes.cpp
+++ src/core/sop/transhuman/definitions/TFCodes.cpp
@@ -3,31 +3,31 @@
 namespace TFCodes {
 	// Phonetic Alphabet/Code declarations
 	namespace PhoneticAlphabetCode {
-		CTerm a = { 'A', "Apex / Alpha" };
-		CTerm b = { 'B', "Boomer / Black" };
-		CTerm c = { 'C', "Charlie" };
-		CTerm d = { 'D', "Delta" };
-		CTerm e = { 'E', "Echo" };
-		CTerm f = { 'F', "Flatline" };
-		CTerm g = { 'G', "None" };
-		CTerm h = { 'H', "Helix" };
-		CTerm i = { 'I', "Ion / Ice" };
-		CTerm j = { 'J', "Jet" };
-		CTerm k = { 'K', "Kilo" };
-		CTerm l = { 'L', "None" };
-		CTerm m = { 'M', "Mace" };
-		CTerm n = { 'N', "Nova / Null" };
-		CTerm o = { 'O', "None" };
-		CTerm p = { 'P', "Payback" };
-		CTerm q = { 'Q', "Quicksand" };
-		CTerm r = { 'R', "Ripcord" };
-		CTerm s = { 'S', "Sundown" };
-		CTerm t = { 'T', "Tango" };
-		CTerm u = { 'U', "Uniform" };
-		CTerm v = { 'V', "Vector" };
-		CTerm w = { 'W', "None" };
-		CTerm x = { 'X', "None" };
-		CTerm y = { 'Y', "None" };
-		CTerm z = { 'Z', "None" };
+		CTerm a = { "A", "Apex / Alpha" };
+		CTerm b = { "B", "Boomer / Black" };
+		CTerm c = { "C", "Charlie" };
+		CTerm d = { "D", "Delta" };
+		CTerm e = { "E", "Echo" };
+		CTerm f = { "F", "Flatline" };
+		CTerm g = { "G", "None" };
+		CTerm h = { "H", "Helix" };
+		CTerm i = { "I", "Ion / Ice" };
+		CTerm j = { "J", "Jet" };
+		CTerm k = { "K", "Kilo" };
+		CTerm l = { "L", "None" };
+		CTerm m = { "M", "Mace" };
+		CTerm n = { "N", "Nova / Null" };
+		CTerm o = { "O", "None" };
+		CTerm p = { "P", "Payback" };
+		CTerm q = { "Q", "Quicksand" };
+		CTerm r = { "R", "Ripcord" };
+		CTerm s = { "S", "Sundown" };
+		CTerm t = { "T", "Tango" };
+		CTerm u = { "U", "Uniform" };
+		CTerm v = { "V", "Vector" };
+		CTerm w = { "W", "None" };
+		CTerm x = { "X", "None" };
+		CTerm y = { "Y", "None" };
+		CTerm z = { "Z", "None" };
 	}
 }
