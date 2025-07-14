176aa9f5af388e4b9f3c866eb57a038ed0382fb5
cpap7 <mjajarmi2000@outlook.com>
2025-07-13 23:22:11 -0400

    Added deployment protocol declarations

--- src/core/sop/transhuman/definitions/DeploymentProtocols.h
+++ src/core/sop/transhuman/definitions/DeploymentProtocols.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "../classes/CProtocol.h"
 
-namespace DeploymentProtocols {
+namespace Protocols {
+	namespace Deployment {
+		extern CProtocol lockdown_procedure;
+		extern CProtocol containment_procedure;
+		extern CProtocol sweep_procedure;
+		extern CProtocol hvt_raid;
+		extern CProtocol priority_protection_detail;
+		extern CProtocol special_deployment;
+		extern CProtocol disengaged_workforce_sterilization;
+		extern CProtocol exceptions;
+		extern CProtocol specialized_deployment_restrictions;
+		extern CProtocol command_exemptions;
+		extern CProtocol stalker_protocols;
+	}
 
 }
