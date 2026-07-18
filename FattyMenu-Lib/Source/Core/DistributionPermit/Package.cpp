26bff7e15b81e5e5e1981f603ce4bf4d98fd9bdd
cpap7 <mjajarmi2000@outlook.com>
2026-07-18 13:27:30 -0400

    Added quantity member to CPackage with a default value of 5

--- FattyMenu-Lib/Source/Core/DistributionPermit/Package.cpp
+++ FattyMenu-Lib/Source/Core/DistributionPermit/Package.cpp
@@ -2,10 +2,8 @@
 
 
 namespace FattyMenu {
-
-	CPackage::CPackage(const char* a_name, int a_cost) 
-		: m_name(a_name), m_cost(a_cost) {
-
+	CPackage::CPackage(const char* a_name, int a_cost, int a_quantity) 
+		: m_name(a_name), m_cost(a_cost), m_quantity(a_quantity) {
 	}
 
 }
--- FattyMenu-Lib/Source/Core/DistributionPermit/Package.h
+++ FattyMenu-Lib/Source/Core/DistributionPermit/Package.h
@@ -5,18 +5,21 @@ namespace FattyMenu {
 	private: 
 		const char* m_name	= "";
 		int m_cost			= 0;
+		int m_quantity		= 5; // NOTE: This is the same across all packages as of now
 
 	public:
-		CPackage(const char* a_name, int a_cost);
+		CPackage(const char* a_name, int a_cost, int a_quantity = 5);
 		CPackage() = default;
 
 		~CPackage() = default;
 
 		// Accessors
-		inline void SetPackageName(const char* a_name)	{ m_name = a_name; }
-		inline void SetPackageCost(int a_cost)			{ m_cost = a_cost; }
+		inline void SetName(const char* a_name)	{ m_name = a_name;			}
+		inline void SetCost(int a_cost)			{ m_cost = a_cost;			}
+		inline void SetQuantity(int a_quantity)	{ m_quantity = a_quantity;	} // Optional -- added for completeness
 
-		inline const char* GetPackageName() const	{ return m_name; }
-		inline int GetPackageCost() const			{ return m_cost; }
+		inline const char* GetName() const		{ return m_name;			}
+		inline int GetCost() const				{ return m_cost;			}
+		inline int GetQuantity() const			{ return m_quantity;		}
 	};
 }
