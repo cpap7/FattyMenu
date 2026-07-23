db9bde21d62812e2fdb61e334f86aec385995f8e
cpap7 <mjajarmi2000@outlook.com>
2026-07-23 15:16:27 -0400

    Reworked CVoiceline class: - Removed EPlayerFactionCategory enum entirely.
    - Refactored EPlayerFactionType enum to EFaction, and added bitwise operator overloads.
    
    - Reworked string functions to return strings with spacing between words (i.e., CivilProtection is now Civil Protection)

--- FattyMenu-Lib/Source/Core/Voicelines/Voiceline.cpp
+++ FattyMenu-Lib/Source/Core/Voicelines/Voiceline.cpp
@@ -2,10 +2,8 @@
 
 namespace FattyMenu {
 
-	CVoiceline::CVoiceline(const char* a_command, const char* a_full_voiceline, 
-		EPlayerFactionType a_player_faction_type, EPlayerFactionCategory a_player_faction_category)
-		: m_command(a_command), m_full_voiceline(a_full_voiceline), 
-		m_player_faction_type(a_player_faction_type), m_player_faction_category(a_player_faction_category) {
+	CVoiceline::CVoiceline(const std::string& a_command, const std::string& a_full_voiceline, EFaction a_factions)
+		: m_command(a_command), m_full_voiceline(a_full_voiceline), m_factions(a_factions) {
 
 	}
 
--- FattyMenu-Lib/Source/Core/Voicelines/Voiceline.h
+++ FattyMenu-Lib/Source/Core/Voicelines/Voiceline.h
@@ -1,140 +1,179 @@
 #pragma once
+#include "../../Base.h"
 
 #include <string>
+#include <vector>
+#include <type_traits>
 
 namespace FattyMenu {
 
-	// Player faction types
-	enum class EPlayerFactionType : uint16_t {
+	// Player faction bitwise flags
+	enum class EFaction : uint16_t {
 		None					= 0,
 		
-		Airwatch				= 1,
-		CivilProtection			= 2,
-		FemaleCitizen			= 3,
-		MaleCitizen				= 4,
-		TranshumanCharger		= 8,
-		TranshumanGrunt			= 5,
-		TranshumanOrdinal		= 6,
-		TranshumanSoldier		= 7,
-		TranshumanSuppressor	= 9,
-		Vortigaunt				= 10,
+		Airwatch				= BIT(0),
+		CivilProtection			= BIT(1),
+		FemaleCitizen			= BIT(2),
+		MaleCitizen				= BIT(3),
+		TranshumanCharger		= BIT(4),
+		TranshumanGrunt			= BIT(5),
+		TranshumanOrdinal		= BIT(6),
+		TranshumanSoldier		= BIT(7),
+		TranshumanSuppressor	= BIT(8),
+		Vortigaunt				= BIT(9),
 		
 		Default					= None
 	};
 
-	std::string PlayerFactionToString(EPlayerFactionType a_player_faction) {
-		switch (a_player_faction) {
-			case EPlayerFactionType::None:					break;
+	// EFaction operator overloads for bit operations (i.e., OR, AND)
 
-			case EPlayerFactionType::Airwatch:				return "Airwatch";
-			case EPlayerFactionType::CivilProtection:		return "CivilProtection";
-			case EPlayerFactionType::FemaleCitizen:			return "FemaleCitizen";
-			case EPlayerFactionType::MaleCitizen:			return "MaleCitizen";
-			case EPlayerFactionType::TranshumanCharger:		return "TranshumanCharger";
-			case EPlayerFactionType::TranshumanGrunt:		return "TranshumanGrunt";
-			case EPlayerFactionType::TranshumanOrdinal:		return "TranshumanOrdinal";
-			case EPlayerFactionType::TranshumanSoldier:		return "TranshumanSoldier";
-			case EPlayerFactionType::TranshumanSuppressor:	return "TranshumanSuppressor";
-			case EPlayerFactionType::Vortigaunt:			return "Vortigaunt";
+	constexpr EFaction operator|(EFaction a_lhs, EFaction a_rhs) {
+		using underlying = std::underlying_type_t<EFaction>;
 
-			default:										break;
+		return static_cast<EFaction>(static_cast<underlying>(a_lhs) | static_cast<underlying>(a_rhs));
+	}
+
+	constexpr EFaction& operator|=(EFaction& a_lhs, EFaction a_rhs) {
+		a_lhs = a_lhs | a_rhs;
+		return a_lhs;
+	}
+
+	constexpr EFaction operator&(EFaction a_lhs, EFaction a_rhs) {
+		using underlying = std::underlying_type_t<EFaction>;
+
+		return static_cast<EFaction>(static_cast<underlying>(a_lhs) & static_cast<underlying>(a_rhs));
+	}
+
+	constexpr EFaction& operator&=(EFaction& a_lhs, EFaction a_rhs) {
+		a_lhs = a_lhs & a_rhs;
+		return a_lhs;
+	}
+
+	constexpr EFaction operator~(EFaction a_value) {
+		using underlying = std::underlying_type_t<EFaction>;
+
+		return static_cast<EFaction>(~static_cast<underlying>(a_value));
+	}
+
+	constexpr bool HasFaction(EFaction a_value, EFaction a_flag) {
+		return (a_value & a_flag) == a_flag;
+	}
+
+	// String conversion helpers for single faction bits
+
+	// Converts a single faction bit to its corresponding string 
+	inline std::string FactionToString(EFaction a_faction) {
+		switch (a_faction) {
+			case EFaction::None:					break;
+
+			case EFaction::Airwatch:				return "Airwatch";
+			case EFaction::CivilProtection:			return "Civil Protection";
+			case EFaction::FemaleCitizen:			return "Female Citizen";
+			case EFaction::MaleCitizen:				return "Male Citizen";
+			case EFaction::TranshumanCharger:		return "Transhuman Charger";
+			case EFaction::TranshumanGrunt:			return "Transhuman Grunt";
+			case EFaction::TranshumanOrdinal:		return "Transhuman Ordinal";
+			case EFaction::TranshumanSoldier:		return "Transhuman Soldier";
+			case EFaction::TranshumanSuppressor:	return "Transhuman Suppressor";
+			case EFaction::Vortigaunt:				return "Vortigaunt";
+
+			default:								break;
 
 		}
 
 		return "None";
 	}
 
-	EPlayerFactionType PlayerFactionFromString(const std::string& a_player_faction_string) {
-		if (a_player_faction_string == "Airwatch")					{ return EPlayerFactionType::Airwatch;				}
-		else if (a_player_faction_string == "CivilProtection")		{ return EPlayerFactionType::CivilProtection;		}
-		else if (a_player_faction_string == "FemaleCitizen")		{ return EPlayerFactionType::FemaleCitizen;			}
-		else if (a_player_faction_string == "MaleCitizen")			{ return EPlayerFactionType::MaleCitizen;			}
-		else if (a_player_faction_string == "TranshumanCharger")	{ return EPlayerFactionType::TranshumanCharger;		}
-		else if (a_player_faction_string == "TranshumanGrunt")		{ return EPlayerFactionType::TranshumanGrunt;		}
-		else if (a_player_faction_string == "TranshumanOrdinal")	{ return EPlayerFactionType::TranshumanOrdinal;		}
-		else if (a_player_faction_string == "TranshumanSoldier")	{ return EPlayerFactionType::TranshumanSoldier;		}
-		else if (a_player_faction_string == "TranshumanSuppressor") { return EPlayerFactionType::TranshumanSuppressor;	}
-		else if (a_player_faction_string == "Vortigaunt")			{ return EPlayerFactionType::Vortigaunt;			}
+	// Converts a single faction token to its corresponding faction bit
+	inline EFaction FactionFromString(const std::string& a_faction_string) {
+		if (a_faction_string == "Airwatch")						{ return EFaction::Airwatch;				}
+		else if (a_faction_string == "Civil Protection")		{ return EFaction::CivilProtection;			}
+		else if (a_faction_string == "Female Citizen")			{ return EFaction::FemaleCitizen;			}
+		else if (a_faction_string == "Male Citizen")			{ return EFaction::MaleCitizen;				}
+		else if (a_faction_string == "Transhuman Charger")		{ return EFaction::TranshumanCharger;		}
+		else if (a_faction_string == "Transhuman Grunt")		{ return EFaction::TranshumanGrunt;			}
+		else if (a_faction_string == "Transhuman Ordinal")		{ return EFaction::TranshumanOrdinal;		}
+		else if (a_faction_string == "Transhuman Soldier")		{ return EFaction::TranshumanSoldier;		}
+		else if (a_faction_string == "Transhuman Suppressor")	{ return EFaction::TranshumanSuppressor;	}
+		else if (a_faction_string == "Vortigaunt")				{ return EFaction::Vortigaunt;				}
 
-		return EPlayerFactionType::None;
+		return EFaction::None;
 	}
-
-	enum class EPlayerFactionCategory : uint16_t {
-		None	= 0,
-
-		Combine = 1,	// Airwatch, Civil Protection, Transhuman factions
-		Citizen = 2,	// Female & Male citizen factions
-		Other	= 3,	// Vortigaunt faction (and Wallace Breen - voicelines exist but it's unused in game)
-
-		Default = None
+	
+	// Lookup table - Canonical list of all faction bits for the serializer & converter to iterate through
+	inline constexpr EFaction c_voiceline_faction_flags[] = {
+		EFaction::Airwatch,
+		EFaction::CivilProtection,
+		EFaction::FemaleCitizen,
+		EFaction::MaleCitizen,
+		EFaction::TranshumanCharger,
+		EFaction::TranshumanGrunt,
+		EFaction::TranshumanOrdinal,
+		EFaction::TranshumanSoldier,
+		EFaction::TranshumanSuppressor,
+		EFaction::Vortigaunt
 	};
 
-	std::string PlayerFactionCategoryToString(EPlayerFactionCategory a_faction_category) {
-		switch (a_faction_category) {
-			case EPlayerFactionCategory::None:					break;
-			
-			case EPlayerFactionCategory::Combine:				return "Combine";
-			case EPlayerFactionCategory::Citizen:				return "Citizen";
-			case EPlayerFactionCategory::Other:					return "Other";
-			
-			default:											break;
+	// Expands a combined bitmask into a list of faction name strings
+	inline std::vector<std::string> FactionMaskToStrings(EFaction a_mask) {
+		std::vector<std::string> faction_names;
+
+		for (EFaction flag : c_voiceline_faction_flags) {
+			if (HasFaction(a_mask, flag)) {
+				faction_names.push_back(FactionToString(flag)); // Append to vector if found
+			}
 		}
 
-		return "None";
+		return faction_names;
 	}
 
-	EPlayerFactionCategory PlayerFactionCategoryFromString(const std::string& a_player_faction_category_string) {
-		if (a_player_faction_category_string == "Combine")		{ return EPlayerFactionCategory::Combine;	}
-		else if (a_player_faction_category_string == "Citizen") { return EPlayerFactionCategory::Citizen;	}
-		else if (a_player_faction_category_string == "Other")	{ return EPlayerFactionCategory::Other;		}
+	// Builds a combined bitmask by ORing each faction token in a list
+	inline EFaction FactionMaskFromStrings(const std::vector<std::string>& a_names) {
+		EFaction faction_mask = EFaction::None;
+		
+		for (const std::string& name : a_names) {
+			faction_mask |= FactionFromString(name);
+		}
 
-		return EPlayerFactionCategory::None;
+		return faction_mask;
 	}
 
 	class CVoiceline {
 	private:
-		const char* m_command								= "";	// Text to be copied to user's clipboard
-		const char* m_full_voiceline						= "";	// Entire voiceline to be displayed next to the command
+		std::string m_command				= "";	// Text to be copied to user's clipboard
+		std::string m_full_voiceline		= "";	// Entire voiceline to be displayed next to the command
 
 		// For filtering
-		EPlayerFactionType m_player_faction_type			= EPlayerFactionType::Default;
-		EPlayerFactionCategory m_player_faction_category	= EPlayerFactionCategory::Default;
+		EFaction m_factions					= EFaction::Default;
 
 	public:
-		CVoiceline(const char* a_command, const char* a_full_voiceline, 
-			EPlayerFactionType a_player_faction_type = EPlayerFactionType::Default, EPlayerFactionCategory a_player_faction_category = EPlayerFactionCategory::Default);
+		CVoiceline(const std::string& a_command, const std::string& a_full_voiceline, EFaction a_factions = EFaction::Default);
 		CVoiceline() = default;
 
 		// Destructor
 		~CVoiceline() = default;
 
 		// Setters
-		inline void SetCommand(const char* a_command)								{ m_command				= a_command;										}
-		inline void SetFullVoiceline(const char* a_full_voiceline)					{ m_full_voiceline		= a_full_voiceline;									}
-		inline void SetPlayerFactionType(EPlayerFactionType a_player_faction_type)	{ m_player_faction_type	= a_player_faction_type;							}
+		inline void SetCommand(const std::string& a_command)									{ m_command					= a_command;							}
+		inline void SetFullVoiceline(const std::string& a_full_voiceline)						{ m_full_voiceline			= a_full_voiceline;						}
+		inline void SetFactions(EFaction a_factions)											{ m_factions				= a_factions;							}
 
 		// Getters
-		inline const char* GetCommand() const										{ return m_command;															}
-		inline const char* GetFullVoiceline() const									{ return m_full_voiceline;													}
-		inline EPlayerFactionType GetPlayerFactionType() const						{ return m_player_faction_type;												}
-		
+		inline const std::string& GetCommand() const											{ return m_command;													}
+		inline const std::string& GetFullVoiceline() const										{ return m_full_voiceline;											}
+		inline EFaction GetFactions() const														{ return m_factions;												}
+
 		// Filtering helpers
 		// Player faction
-		inline bool IsForAirwatch() const											{ return m_player_faction_type == EPlayerFactionType::Airwatch;				}
-		inline bool IsForCivilProtection() const									{ return m_player_faction_type == EPlayerFactionType::CivilProtection;		}
-		inline bool IsForFemaleCitizen() const										{ return m_player_faction_type == EPlayerFactionType::FemaleCitizen;		}
-		inline bool IsForMaleCitizen() const										{ return m_player_faction_type == EPlayerFactionType::MaleCitizen;			}
-		inline bool IsForTranshumanCharger() const									{ return m_player_faction_type == EPlayerFactionType::TranshumanCharger;	}
-		inline bool IsForTranshumanGrunt() const									{ return m_player_faction_type == EPlayerFactionType::TranshumanGrunt;		}
-		inline bool IsForTranshumanOrdinal() const									{ return m_player_faction_type == EPlayerFactionType::TranshumanOrdinal;	}
-		inline bool IsForTranshumanSoldier() const									{ return m_player_faction_type == EPlayerFactionType::TranshumanSoldier;	}
-		inline bool IsForTranshumanSuppressor() const								{ return m_player_faction_type == EPlayerFactionType::TranshumanSuppressor; }
-		inline bool IsForVortigaunt() const											{ return m_player_faction_type == EPlayerFactionType::Vortigaunt;			}
-
-		// Category
-		inline bool IsForCombineCategory() const									{ return m_player_faction_category == EPlayerFactionCategory::Combine;		}
-		//inline bool IsForCitizenCategory() const									{ return IsForFemaleCitizen() || IsForMaleCitizen();						}
-		inline bool IsForCitizenCategory() const									{ return m_player_faction_category == EPlayerFactionCategory::Citizen;		}
-		inline bool IsForOtherCategory() const										{ return m_player_faction_category == EPlayerFactionCategory::Other;		}
+		inline bool IsForAirwatch() const														{ return HasFaction(m_factions, EFaction::Airwatch);				}
+		inline bool IsForCivilProtection() const												{ return HasFaction(m_factions, EFaction::CivilProtection);			}
+		inline bool IsForFemaleCitizen() const													{ return HasFaction(m_factions, EFaction::FemaleCitizen);			}
+		inline bool IsForMaleCitizen() const													{ return HasFaction(m_factions, EFaction::MaleCitizen);				}
+		inline bool IsForTranshumanCharger() const												{ return HasFaction(m_factions, EFaction::TranshumanCharger);		}
+		inline bool IsForTranshumanGrunt() const												{ return HasFaction(m_factions, EFaction::TranshumanGrunt);			}
+		inline bool IsForTranshumanOrdinal() const												{ return HasFaction(m_factions, EFaction::TranshumanOrdinal);		}
+		inline bool IsForTranshumanSoldier() const												{ return HasFaction(m_factions, EFaction::TranshumanSoldier);		}
+		inline bool IsForTranshumanSuppressor() const											{ return HasFaction(m_factions, EFaction::TranshumanSuppressor);	}
+		inline bool IsForVortigaunt() const														{ return HasFaction(m_factions, EFaction::Vortigaunt);				}
 	};
 }
