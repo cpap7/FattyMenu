4ea8e75a0e0de8fbd6d5ee253aa4371c08f45a77
cpap7 <mjajarmi2000@outlook.com>
2026-07-24 18:40:27 -0400

    CPackage & CPermit now have std::string members instead of const char*

--- FattyMenu-Lib/Source/Core/DistributionPermit/Package.cpp
+++ FattyMenu-Lib/Source/Core/DistributionPermit/Package.cpp
@@ -2,7 +2,7 @@
 
 
 namespace FattyMenu {
-	CPackage::CPackage(const char* a_name, int a_cost, int a_quantity) 
+	CPackage::CPackage(const std::string& a_name, int a_cost, int a_quantity)
 		: m_name(a_name), m_cost(a_cost), m_quantity(a_quantity) {
 	}
 
--- FattyMenu-Lib/Source/Core/DistributionPermit/Package.h
+++ FattyMenu-Lib/Source/Core/DistributionPermit/Package.h
@@ -1,25 +1,27 @@
 #pragma once
+#include <string>
 
 namespace FattyMenu {
+
 	class CPackage {
 	private: 
-		const char* m_name	= "";
+		std::string m_name	= "";
 		int m_cost			= 0;
-		int m_quantity		= 5; // NOTE: This is the same across all packages as of now
+		int m_quantity		= 5; // NOTE: Most packages contain item quantities of 5
 
 	public:
-		CPackage(const char* a_name, int a_cost, int a_quantity = 5);
+		CPackage(const std::string& a_name, int a_cost, int a_quantity = 5);
 		CPackage() = default;
 
 		~CPackage() = default;
 
 		// Accessors
-		inline void SetName(const char* a_name)	{ m_name = a_name;			}
-		inline void SetCost(int a_cost)			{ m_cost = a_cost;			}
-		inline void SetQuantity(int a_quantity)	{ m_quantity = a_quantity;	} // Optional -- added for completeness
+		inline void SetName(const std::string& a_name)	{ m_name = a_name;			}
+		inline void SetCost(int a_cost)					{ m_cost = a_cost;			}
+		inline void SetQuantity(int a_quantity)			{ m_quantity = a_quantity;	}
 
-		inline const char* GetName() const		{ return m_name;			}
-		inline int GetCost() const				{ return m_cost;			}
-		inline int GetQuantity() const			{ return m_quantity;		}
+		inline const std::string& GetName() const		{ return m_name;			}
+		inline int GetCost() const						{ return m_cost;			}
+		inline int GetQuantity() const					{ return m_quantity;		}
 	};
 }
