4c326165957338e2639feed4fb3c7daf1b87b337
cpap7 <mjajarmi2000@outlook.com>
2026-08-04 21:05:58 -0400

    Refactored DLLMain. It now outlines the lifetime of CApplication clearly

--- FattyMenu-Lib/Source/FattyMenu/DllMain.cpp
+++ FattyMenu-Lib/Source/FattyMenu/DllMain.cpp
@@ -1,4 +1,5 @@
-#include "Core/Hooks.h"
+//#include "Core/Hooks.h"
+#include "Core/Application.h"
 
 #include <../garrysmod_common/include/GarrysMod/Lua/Interface.h> // Garry's Mod module functions
 
@@ -7,35 +8,25 @@
 
 #include <thread>
 
+bool g_application_running = true;
+
 DWORD WINAPI SetupMainThread(LPVOID a_instance) {
-	try {
-		// Initialize the GUI
-		FattyMenu::GUI::Initialize();
-
-		// Initialize all of the game render-hooks (EndScene/Reset)
-		FattyMenu::Hooks::InitializeHooks();
-
-		FattyMenu::GUI::g_setup_complete = true;
-
-		// Main loop
-		while (!GetAsyncKeyState(VK_END)) {
-			// Sleep thread to save on resources
-			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	if (g_application_running) {
+		try {
+			FattyMenu::CApplication* app = new FattyMenu::CApplication();		// Init
+			app->Run();															// Main loop
+			delete app;															// Application ends
 		}
-
-		// Cleanup
-		FattyMenu::Hooks::DestroyHooks();
-		FattyMenu::GUI::Destroy();
+		catch (const std::exception& ex) {
+			MessageBox(nullptr, ex.what(), "Error", MB_ICONERROR);				// Display the error
+			FreeLibraryAndExitThread(static_cast<HMODULE>(a_instance), 1);		// Exit 1
+		}
 	}
-	catch (const std::exception& ex) {
-		MessageBox(nullptr, ex.what(), "Error", MB_ICONERROR);				// Display the error
-	}
-
+	
 	FreeLibraryAndExitThread(static_cast<HMODULE>(a_instance), 0);
 
 	return 0;
 }
-
 
 // Entry point for GMOD
 // Allows direct interfacing w/ GMOD's LUA API
@@ -50,21 +41,13 @@ GMOD_MODULE_CLOSE() {
 	return 0;
 }
 
-/* Main function / dll entry point
-* @param instance -> instance of the process
-* @param reason -> determines why DllMain is being called
-* @param reserved -> lp void pointer 
-* @return -> TRUE or FALSE 
-*/
+/* Main function / dll entry point */
 BOOL WINAPI DllMain(const HMODULE instance, const std::uintptr_t reason, const void* reserved) {
 
 	switch (reason) {
 		case DLL_PROCESS_ATTACH: {
 			// Stop library calls in the process
 			DisableThreadLibraryCalls(instance);
-
-			// Store the instance handle for manual unloading
-			FattyMenu::GUI::g_module = instance;
 
 			const HANDLE thread = CreateThread(
 				nullptr,
@@ -83,19 +66,9 @@ BOOL WINAPI DllMain(const HMODULE instance, const std::uintptr_t reason, const v
 		}
 
 		case DLL_PROCESS_DETACH: {
-			if (FattyMenu::GUI::g_setup_complete) {
-				// Teardown hooks and GUI
-				FattyMenu::Hooks::DestroyHooks();
-				FattyMenu::GUI::Destroy();
-
-				FattyMenu::GUI::g_setup_complete = false;
-				FattyMenu::GUI::g_initialized = false;
-			}
 			break;
 		}
-			
-	}
-	
 
+	}
 	return TRUE;
 }
